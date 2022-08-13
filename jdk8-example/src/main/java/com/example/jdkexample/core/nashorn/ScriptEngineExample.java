abc28c5ac284c65e2e5aca0ca55fc170fd3ea9f3
nicky <362330721@qq.com>
2022-08-13 14:50:24 +0800

    ScriptEngine example

---
+++ jdk8-example/src/main/java/com/example/jdkexample/core/nashorn/ScriptEngineExample.java
@@ -0,0 +1,65 @@
+package com.example.jdkexample.core.nashorn;
+
+
+import cn.hutool.json.JSONUtil;
+import com.example.jdkexample.entity.User;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.script.*;
+import java.io.*;
+
+@Slf4j
+public class ScriptEngineExample {
+
+    public static void main(String[] args)  throws Exception{
+
+        testAdd();
+        testJson();
+        testJsFile();
+        testInvokeFunction();
+    }
+
+
+    private static void testAdd() throws ScriptException {
+        ScriptEngineManager engineManager = new ScriptEngineManager();
+        ScriptEngine scriptEngine = engineManager.getEngineByName("JavaScript");
+        String foo = "1+2";
+        System.out.println(scriptEngine.eval(foo));
+    }
+
+    private static void testJson() throws ScriptException {
+        String script = "var json ={\"name\":\"admin\" , \"mobiTel\":\"123\"};" +
+                "var result ={};result.name=json.name;result.mobiTel=json.mobiTel;JSON.stringify(result);";
+
+        ScriptEngineManager engineManager = new ScriptEngineManager();
+        ScriptEngine scriptEngine = engineManager.getEngineByName("JavaScript");
+        Object obj = scriptEngine.eval(script);
+        User user = JSONUtil.toBean(obj.toString() , User.class);
+        System.out.println(user.toString());
+    }
+
+    private static void testJsFile()  {
+        try (Reader reader= new FileReader(new File(ScriptEngineExample.class.getClassLoader().getResource("test.js").getFile()))){
+            ScriptEngineManager engineManager = new ScriptEngineManager();
+            ScriptEngine scriptEngine = engineManager.getEngineByName("JavaScript");
+            scriptEngine.eval(reader);
+        } catch (FileNotFoundException e) {
+            log.error("FileNotFoundException：{}" , e);
+        } catch (IOException e) {
+            log.error("IOException：{}" , e);
+        } catch (ScriptException e){
+            log.error("ScriptException：{}" , e);
+        }
+    }
+
+
+    private static void testInvokeFunction() throws ScriptException, NoSuchMethodException, FileNotFoundException {
+        String script = "var json ={\"name\":\"admin\" , \"mobiTel\":\"123\"};" +
+                "function jsonStringify(){ var result ={};result.name=json.name;result.mobiTel=json.mobiTel;print(JSON.stringify(result));}";
+        ScriptEngineManager engineManager = new ScriptEngineManager();
+        ScriptEngine scriptEngine = engineManager.getEngineByName("JavaScript");
+        scriptEngine.eval(script);
+        Invocable invocable = (Invocable) scriptEngine;
+        invocable.invokeFunction("jsonStringify");
+    }
+}
---
+++ jdk8-example/src/main/resources/test.js
@@ -0,0 +1,5 @@
+var json ={"name":"admin" , "mobiTel":"123"};
+var result = {};
+result.name = json.name;
+result.mobiTel = json.mobiTel;
+print(JSON.stringify(result));
